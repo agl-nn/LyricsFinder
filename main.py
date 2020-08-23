8d0eb15ad25b20f8fc62c124c897e18f046e1652
DmitryKuznetsoff <kuznetsoffd90@gmail.com>
2020-08-23 11:23:33 +0300

    methods get_lyrics_from_txt, write_lyrics_to_txt, get_lyrics added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
-import re
+import os
 
 BASE_URL = 'https://api.genius.com/'
 
@@ -9,6 +9,7 @@ BASE_URL = 'https://api.genius.com/'
 class LyricsFinder:
     def __init__(self, token):
         self.genius_oauth = token
+        self.lyrics_dir = os.path.join(os.getcwd(), 'lyrics' + os.sep)
 
     def get_lyrics_url(self, artist, track):
         """
@@ -24,27 +25,58 @@ class LyricsFinder:
         for item in search_result:
             artist_name = item['result']['primary_artist']['name']
             lyrics_url = item['result']['url']
-            if artist.lower().strip() == artist_name.lower():
+            if artist.lower().strip() in artist_name.lower():
                 return lyrics_url
 
-    def get_lyrics(self, artist, track):
+    def get_lyrics_by_url(self, artist, track):
         """
         метод парсит страницу, полученную с помощью get_lyrics_url
         возвращает текст песни
         """
+        result = {}
         url = self.get_lyrics_url(artist, track)
         if not url:
             raise Exception('failed to get url on request')
-        # пишем рягулярное выражение для поиска текста в html:
-        pattern = re.compile(url[18:] + r'#note-\d')
         post_page = requests.get(url)
         soup = BeautifulSoup(post_page.text, 'html.parser')
-        text = soup.find_all(href=pattern)
-        return [string.text for string in text]
+        text = soup.select('[class~=lyrics]')
+        lyrics = text[0].text
+        result.update({'artist': artist, 'track': track, 'lyrics': lyrics})
+        return result
+
+    def write_lyrics_to_txt(self, lyrics):
+        artist = lyrics['artist'].capitalize()
+        track = lyrics['track'].capitalize()
+        title = f'{artist} -- {track}'
+        with open(f'{self.lyrics_dir}{title}.txt', 'w', encoding='utf=8') as f:
+            f.write(title)
+            for line in lyrics['lyrics']:
+                f.write(line)
+
+    def get_lyrics_from_txt(self, artist, track):
+        file_name = f'{artist.capitalize()} -- {track.capitalize()}.txt'
+        dir_content = os.listdir(self.lyrics_dir)
+        if file_name in dir_content:
+            with open(f'{self.lyrics_dir}{file_name}', 'r', encoding='utf-8') as f:
+                lyrics = f.read()
+            return lyrics
+        else:
+            return None
+
+    def get_lyrics(self, artist, track):
+        lyrics_from_txt = self.get_lyrics_from_txt(artist, track)
+        if lyrics_from_txt:
+            print('from get_lyrics_from_txt method')
+            return lyrics_from_txt
+        else:
+            lyrics_by_url = self.get_lyrics_by_url(artist, track)
+            print('from get_lyrics_by_url method')
+            self.write_lyrics_to_txt(lyrics_by_url)
+            return self.get_lyrics_from_txt(artist, track)
 
 
 if __name__ == '__main__':
     TOKEN = ''
     finder = LyricsFinder(TOKEN)
-    lyrics = finder.get_lyrics('nirvana', 'come as you are')
-    pprint(lyrics)
+
+    print(finder.get_lyrics('nirvana', 'come as you are'))
